0b10c14c6391bc0605a94e56c66e1e5faa22ec88
Kevin Jun C Li <kevinjcli1994@gmail.com>
2019-01-24 11:05:03 -0500

    set up gulp and eslint configuration files

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+
+const terser = require('terser'),
+      rename = require('gulp-rename'),
+      eslint = require('eslint'),
+      browserSync = require('browser-sync').create();
+
+gulp.task('eslint', () => {
+  return gulp.src('./app/js/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failtAfterError());
+});
+
+gulp.task('minifyjs', done => {
+  return gulp
+    .src('./app/js/**/*.js')
+    .pipe(terser())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+}); 
+
+gulp.task('watch', () => {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch('./app/js/**/*.js', gulp.series(['eslint', 'minifyjs']));
+  gulp.watch('./app/*html').on('change', browserSync.reload);
+  gulp.watch('');
+});
+
+gulp.task('default', gulp.series('watch'));
