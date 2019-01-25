e74398d5953502a07d0a47b66147a1b5cc286f74
Kevin Jun C Li <kevinjcli1994@gmail.com>
2019-01-25 09:59:44 -0500

    made app javascript fully functional

--- app/js/main.js
+++ app/js/main.js
@@ -7,16 +7,14 @@ $(function(){
     const selectedCategory = $dropDown.val();
     const targetUrl = `https://api.nytimes.com/svc/topstories/v2/${selectedCategory}.json?api-key=fas25Dtm8dhqohIpf8HSFFMv5gyzhoAY`
 
+    $('main').children().remove();
     $('main').append('<ul></ul>');
-
 
     $.ajax({
       method: 'GET',
       url: targetUrl,
     })
     .done(data => {
-      console.log(data);
-      console.log(data.results[1].multimedia[4].url);
       const articles = data.results;
       let articleCounter = 0;
       articles.forEach(article => {
@@ -24,8 +22,6 @@ $(function(){
           articleCounter++;
           $('main ul').append(`<li><a></a></li>`);
           $(`main ul li:nth-child(${articleCounter})`).css('backgroundImage', `url(${article.multimedia[4].url})`)
-
-          console.log(article.multimedia[4].url)
         } 
       })
     });
