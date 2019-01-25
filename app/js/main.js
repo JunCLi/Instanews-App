64c610fa8e36c0f4bf2ab63f600c05b635f38f5a
Kevin Jun C Li <kevinjcli1994@gmail.com>
2019-01-25 09:51:11 -0500

    added ajax call, selection support, style to render page approximate

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,37 @@
 (function($) {
 $(function(){
+  
+  const $dropDown = $('.select-category');
+  
+  $dropDown.on('change', (event) => {
+    const selectedCategory = $dropDown.val();
+    const targetUrl = `https://api.nytimes.com/svc/topstories/v2/${selectedCategory}.json?api-key=fas25Dtm8dhqohIpf8HSFFMv5gyzhoAY`
 
-  console.log('hi');
+    $('main').append('<ul></ul>');
 
-})
+
+    $.ajax({
+      method: 'GET',
+      url: targetUrl,
+    })
+    .done(data => {
+      console.log(data);
+      console.log(data.results[1].multimedia[4].url);
+      const articles = data.results;
+      let articleCounter = 0;
+      articles.forEach(article => {
+        if (article.multimedia.length !== 0 && articleCounter < 12) {
+          articleCounter++;
+          $('main ul').append(`<li><a></a></li>`);
+          $(`main ul li:nth-child(${articleCounter})`).css('backgroundImage', `url(${article.multimedia[4].url})`)
+
+          console.log(article.multimedia[4].url)
+        } 
+      })
+    });
+
+  });
+  
+
+});
 })(jQuery);
