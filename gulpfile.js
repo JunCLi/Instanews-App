aca702af9e7dd2cbdcd3e52d3dcca0e5343e70b7
Kevin Jun C Li <kevinjcli1994@gmail.com>
2019-01-28 15:54:04 -0500

    installed gulp dependencies and completed a working automated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,85 @@
 const gulp = require('gulp');
 
-const terser = require('gulp-terser'),
-      rename = require('gulp-rename'),
-      eslint = require('gulp-eslint'),
+const rename = require('gulp-rename'),
+      terser = require('gulp-terser'),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      cssnano = require('gulp-cssnano'),
+      imagemin = require('gulp-imagemin'),
+      cache = require('gulp-cache'),
+      del = require('del'),
       browserSync = require('browser-sync').create();
 
-gulp.task('eslint', () => {
-  return gulp.src('./app/js/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failtAfterError());
-});
-
-gulp.task('minifyjs', done => {
+// Development Tasks
+gulp.task('sass', () => {
   return gulp
-    .src('./app/js/**/*.js')
-    .pipe(terser())
-    .pipe(rename({extname: '.min.js'}))
-    .pipe(gulp.dest('./build/js'))
+    .src('./app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"]
+    }))
+    .pipe(rename("style.css"))
+    .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
-}); 
+})
 
 gulp.task('watch', () => {
   browserSync.init({
     server: {
-      baseDir: "./app/"
+      baseDir: './app/'
     }
   });
-  gulp.watch('./app/js/**/*.js', gulp.series(['eslint', 'minifyjs']));
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp.watch('');
+  gulp.watch('./app/sass/**/*.scss', gulp.series(['sass'])).on('change', browserSync.reload);
+  gulp.watch('./app/js/**/*.js').on('change', browserSync.reload);
+  gulp.watch('./app/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.series(['watch']))
+
+
+// Distribution Tasks
+gulp.task('importHtml', () => {
+  return gulp.src('./app/*.html')
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('minifycss', () => {
+  return gulp.src('./app/css/**/*.css')
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('minifyjs', () => {
+  return gulp.src('./app/js/**/*.js')
+    .pipe(terser())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', () => {
+  return gulp.src('./app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('./dist/images'))
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('./app/fonts/**/*')
+  .pipe(gulp.dest('./dist/fonts'))
+});
+
+gulp.task('clean:dist', () => {
+  return del(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+gulp.task('cache:clear',  callback => {
+  return cache.clearAll(callback)
+});
+
+gulp.task('build', gulp.series(['clean:dist', gulp.parallel(['importHtml', 'sass', 'minifycss', 'minifyjs', 'images', 'fonts'])]), () => {
+  console.log('Building Files');
+});
